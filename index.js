940d78143289bb4eb72f7b6e5f84a2cc56221013
Lucas Gomes <lucasagostini400@gmail.com>
2024-08-25 15:44:26 -0300

    Significado de operadores lógicos no Javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+    == ----- igual ----- a==b ----- verdadeiro se a for igula ao b
+    
+    === ---- identico -- a === b -- verdadeiro se a for identico ao b
+    
+    != ----- diferente - a != b --- verdadeiro se o a for diferente de b
+    
+    !== ---- não identico - a !== b - verdadeiro se o a não for identico ao b
+
+    < ------ menor que - a < b ---- verdadeiro quando a for menor que b
+    
+    <= ----- menor ou igual que - a <= b---- verdeiro quando a for menor ou igual que b
+    
+    > ------ maior que - a > b ---- verdadeiro quando a for maior que b
+    > ------ maior ou igual que - a >= b ---- verdadeiro quando a for maior ou igual que b
+
+*/
+
+const a = 10
+const b = 3
+
+console.log(a >= b)
