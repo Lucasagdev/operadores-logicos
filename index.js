5c18b97b5bccbf63743329c35156b9d96bf31951
Lucas Gomes <lucasagostini400@gmail.com>
2024-08-26 21:21:49 -0300

    Aprendendo sobre mais operadores lógicos no Javascript

--- index.js
+++ index.js
@@ -20,3 +20,42 @@ const a = 10
 const b = 3
 
 console.log(a >= b)
+
+const a = 2
+const b = 2
+
+console.log(a === b && a <= b)
+// V e V = V
+
+console.log(a === b && a < b)
+// V e F = F
+
+console.log(a > b && a === b)
+// F e V = F
+
+console.log(a > b && a < b)
+// F e F = F
+
+/*
+    OR  ||
+*/
+
+console.log(a === b || a <= b)
+// V ou V = V
+
+console.log(a === b || a < b)
+// V ou F = V
+
+console.log(a > b || a === b)
+// F ou V = V
+
+console.log(a > b || a < b)
+// F ou F = F
+
+/*
+    NOT
+*/
+
+console.log(!(a === b))
+
+console.log(a < b)
